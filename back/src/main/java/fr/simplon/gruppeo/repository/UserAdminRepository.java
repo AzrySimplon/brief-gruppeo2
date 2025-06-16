5e4838713caf30fe8b6e0bdce35cb0dfcf4f33dd
AzrySimplon <azriel.simplon@gmail.com>
2025-06-16 16:09:15 +0200

    Remove `UserAdmin`, `UserViewer`, related controllers, repositories, and tests.

--- back/src/main/java/fr/simplon/gruppeo/repository/UserAdminRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.simplon.gruppeo.repository;
-
-import fr.simplon.gruppeo.model.UserAdmin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAdminRepository extends JpaRepository<UserAdmin, Long> {
-}
